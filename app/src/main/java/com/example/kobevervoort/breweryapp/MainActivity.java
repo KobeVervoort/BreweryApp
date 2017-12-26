f98df5654e80cbd764df1ac39c6e9290b34b2569
Helena Sollie <helena.sollie@gmail.com>
2017-12-26 16:21:43 +0100

    Asynchtask  + data in array

--- app/src/main/java/com/example/kobevervoort/breweryapp/MainActivity.java
+++ app/src/main/java/com/example/kobevervoort/breweryapp/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.kobevervoort.breweryapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EventObject;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    public static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = (Button)findViewById(R.id.button);
+        data = (TextView)findViewById(R.id.fetcheddata);
+
+        BufferedReader reader = null;
+
+        click.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                fetchData process = new fetchData();
+                process.execute();
+
+            }
+        });
+
+
+
     }
+
+
 }
+
---
+++ app/src/main/java/com/example/kobevervoort/breweryapp/fetchData.java
@@ -0,0 +1,91 @@
+package com.example.kobevervoort.breweryapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Helena on 26/12/17.
+ */
+
+public class fetchData extends AsyncTask<Void, Void, String> {
+
+    String data ="";
+    String dataParsed = "";
+    String singleParsed ="";
+
+    protected void onPreExecute() {
+
+    }
+
+    protected String doInBackground(Void... urls) {
+
+        try {
+
+            URL url = new URL("http://beermapping.com/webservice/locquery/f4ac5bafde4e3ca58d904d266fe956ca/piece&s=json");
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    stringBuilder.append(line).append("\n");
+                    data = data + line;
+                }
+
+                JSONArray JA = new JSONArray(data);
+                for (int i = 0; i<JA.length(); i++) {
+
+                    JSONObject JO = (JSONObject) JA.get(i);
+                    singleParsed = "Name: " + JO.get("name") + "\n" +
+                            "City: " + JO.get("city") + "\n" +
+                            "Status: " + JO.get("status") + "\n" ;
+
+                    dataParsed = dataParsed + singleParsed;
+
+                }
+
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+
+            finally{
+
+                urlConnection.disconnect();
+            }
+        }
+
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+
+        MainActivity.data.setText(this.dataParsed);
+
+
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        Log.i("INFO", response);
+    }
+
+
+
+
+}
